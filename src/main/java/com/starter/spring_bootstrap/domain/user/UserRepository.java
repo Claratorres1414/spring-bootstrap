8f14fdf34f57552f0ee2e327c2a13109efc236b8
Claratorres1414 <claratorres12345679@gmail.com>
2026-08-27 20:08:18 -0300

    feat: cria user-repository

---
+++ src/main/java/com/starter/spring_bootstrap/domain/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.starter.spring_bootstrap.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
