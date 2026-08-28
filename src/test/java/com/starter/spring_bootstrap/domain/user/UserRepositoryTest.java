ce14706b1d6cdc33f9c9092f38f04ce0f9cb16c6
Claratorres1414 <claratorres12345679@gmail.com>
2026-08-28 14:26:08 -0300

    feat: adiciona teste para constraint de que username deve ser único

--- src/test/java/com/starter/spring_bootstrap/domain/user/UserRepositoryTest.java
+++ src/test/java/com/starter/spring_bootstrap/domain/user/UserRepositoryTest.java
@@ -3,12 +3,12 @@ package com.starter.spring_bootstrap.domain.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -30,5 +30,22 @@ class UserRepositoryTest {
         assertNotNull(savedUser.getCreatedAt());
         assertNotNull(savedUser.getUpdatedAt());
         assertEquals("test-user", savedUser.getUsername());
+    }
+
+    @Test
+    void shouldNotPersistUserWithDuplicateUsername() {
+        User firstUser = User.builder()
+                .username("test-user")
+                .build();
+
+        User secondUser = User.builder()
+                .username("test-user")
+                .build();
+
+        userRepository.saveAndFlush(firstUser);
+
+        assertThrows(DataIntegrityViolationException.class, () ->
+                userRepository.saveAndFlush(secondUser)
+        );
     }
 }
